d4dbc5da012936cf5498f72f06974374160fd7a6
Creepinator45 <creepinator45@gmail.com>
2025-02-07 17:03:26 -0800

    Implemented logic for a single game

--- projects/project1/program.py
+++ projects/project1/program.py
@@ -64,15 +64,15 @@ class MultiDeck:
         return chosenCard
 
 class Player:
-    def __init__(self, name: str, hand: Iterable[Card] = []):
+    def __init__(self, name: str):
         self.__name: str = name
-        self.__hand: list[Card] = list(hand)
-        self.__handValue: int = Player.calculateHand(hand = self.__hand)
+        self.__hand: list[Card] = []
+        self.__handValue: int = 0
 
     def status(self, open: bool = True) -> str:
         """
         return string representation of the player's hand, in the form of "{self.__name}'s hand is {hand}, and the total is {self.__handValue}"
-        if open is true will return all cards, if open is false will obscure the first card
+        if open is true will return all cards, if open is false will obscure the first card. Will not include total if open is false
         O(n) for cards in hand
         """
         #I would have used __str__ for this, but I wanted it to be passed the parameter for whether to show or conceal the first card
@@ -83,7 +83,10 @@ class Player:
             hiddenCard = "face down"
         hand = ", ".join([str(card) for card in [hiddenCard] + self.__hand[1:]])
 
-        return f"{self.__name}'s hand is {hand}, and the total is {self.__handValue}"
+        if open:
+            return f"{self.__name}'s hand is {hand}, and the total is {self.__handValue}"
+        else:
+            return f"{self.__name}'s hand is {hand}"
 
     @property
     def handValue(self) -> int:
@@ -127,6 +130,11 @@ class Player:
         self.__hand.append(card)
         #This could be made O(1) by calculating the new value based on the previous value, instead of recalculating it from scratch
         self.__handValue = Player.calculateHand(self.__hand)
+    
+    def clearHand(self) -> None:
+        self.__hand = []
+        self.__handValue = 0
+
 
 class Game:
     def __init__(self, player1_name:str = "Player", player2_name:str = "Dealer"):
@@ -138,14 +146,17 @@ class Game:
         Begins and runs a game of Bag Jack
         """
 
-        #initializing game
-        print(f"{self.__player1.name} is playing Bag Jack against {self.__player2.name}")
+        #setting up the game
+        self.__player1.clearHand()
+        self.__player2.clearHand()
 
         #randomly selects number of decks from 2, 4, 6, and 8 if none is supplied
         if numDecks is None:
             numDecks = 2 * randint(1, 4)
 
         deck = MultiDeck(numDecks=numDecks)
+
+        print(f"{self.__player1.name} is playing Bag Jack against {self.__player2.name}. Dealing from {numDecks} decks")
 
         #drawing starting hands
         self.__player1.addCard(deck.dealCard())
@@ -155,14 +166,65 @@ class Game:
         self.__player2.addCard(deck.dealCard())
         self.__player2.addCard(deck.dealCard())
         print(self.__player2.status(open=False))
+        #todo check for blackjacks
 
+        #player's turn
+        print(f"{self.__player1.name}'s turn")
+        while True:
+            input_choice = input("Hit (h) or Stay (s): ")
+            match input_choice:
+                case "h":
+                    self.__player1.addCard(deck.dealCard())
+                    print(self.__player1.status(open=True))
+
+                    #exit game if busted
+                    if self.__player1.handValue > 21:
+                        print(f"{self.__player1.name} busted. {self.__player2.name} wins!")
+                        return
+                    #end turn if 21
+                    if self.__player1.handValue == 21:
+                        break
+                    continue
+                case "s":
+                    #end turn
+                    break
+                case _:
+                    #ask for input again if not give "h" or "s"
+                    print(f"{input_choice} was not a valid decision")
+                    continue
+        print(self.__player1.status(open=True))
+
+        #CPU's turn
+        print(f"{self.__player2.name}'s turn")
+        print(self.__player2.status(open=True))
+        while self.__player2.handValue < 17:
+            print(f"{self.__player2.name} takes a card")
+            self.__player2.addCard(deck.dealCard())
+            print(self.__player2.status(open=True))
+
+            #exit game if busted
+            if self.__player2.handValue > 21:
+                print(f"{self.__player2.name} busted. {self.__player1.name} wins!")
+                return
+        print(f"{self.__player2.name} stays")
+
+        #print the final hands
+        print(self.__player1.status(True))
+        print(self.__player2.status(True))
+
+        #check for winner
+        if self.__player1.handValue > self.__player2.handValue:
+            print(f"{self.__player1.name} wins!")
+        elif self.__player2.handValue > self.__player1.handValue:
+            print(f"{self.__player2.name} wins!")
+        else:
+            print("Tie!")
+        return
 
 def main():
     print("Hello, World!")
     game = Game()
     game.beginGame()
-
-
 
 if __name__ == '__main__':
     main()
