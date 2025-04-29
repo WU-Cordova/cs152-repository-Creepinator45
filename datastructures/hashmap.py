88e23f4f798374c2098a5451cd58e6373c5c4876
Creepinator45 <creepinator45@gmail.com>
2025-04-28 22:43:31 -0700

    implemented hashmap

--- datastructures/hashmap.py
+++ datastructures/hashmap.py
@@ -4,40 +4,94 @@ from datastructures.ihashmap import KT, VT, IHashMap
 from datastructures.array import Array
 import pickle
 import hashlib
-
 from datastructures.linkedlist import LinkedList
+from datastructures.primesieve import PrimeSieve
 
 class HashMap(IHashMap[KT, VT]):
+    prime_sieve = PrimeSieve()
+
+    class ChainLink:
+
+        def __init__(self, key: KT, val: VT):
+            self.__key = key
+            self.__val = val
+        
+        @property
+        def key(self) -> KT:
+            return self.__key
+        
+        @property
+        def val(self) -> VT:
+            return self.__val
+        @val.setter
+        def val(self, val: VT):
+            self.__val = val
+        
+        def __eq__(self, other: "HashMap.ChainLink") -> bool:
+            return self.key == other.key and self.val == other.val
 
     def __init__(self, number_of_buckets=7, load_factor=0.75, custom_hash_function: Optional[Callable[[KT], int]]=None) -> None:
-        raise NotImplementedError("HashMap.__init__() is not implemented yet.")
+        self.__buckets: Array[LinkedList[HashMap.ChainLink]] = \
+            Array([LinkedList(data_type=HashMap.ChainLink) for _ in range(number_of_buckets)], data_type=LinkedList)
+        self.__count: int = 0
+        self.__load_factor_threshold: float = load_factor
+        self.__hash_function = custom_hash_function if custom_hash_function is not None else HashMap.__default_hash_function
+        self.__prime_iter = iter(HashMap.prime_sieve)
 
     def __getitem__(self, key: KT) -> VT:
-        raise NotImplementedError("HashMap.__getitem__() is not implemented yet.")
+        chain: LinkedList[HashMap.ChainLink] = self.__buckets[self.__get_bucket_index(key)]
+        for link in chain:
+            if link.key == key:
+                return link.val
+        raise KeyError(f"{key} not in HashMap")
 
-    def __setitem__(self, key: KT, value: VT) -> None:        
-        raise NotImplementedError("HashMap.__setitem__() is not implemented yet.")
+    def __setitem__(self, key: KT, value: VT) -> None:
+        if len(self) >= self.__load_factor_threshold * len(self.__buckets):
+            self.__resize()
+
+        chain: LinkedList[HashMap.ChainLink] = self.__buckets[self.__get_bucket_index(key)]
+        for link in chain:
+            if link.key == key:
+                link.val = value
+                return
+        chain.append(HashMap.ChainLink(key, value))
+        self.__count += 1
 
     def keys(self) -> Iterator[KT]:
-        raise
+        for chain in self.__buckets:
+            for link in chain:
+                yield link.key
     
     def values(self) -> Iterator[VT]:
-        raise NotImplementedError("HashMap.values() is not implemented yet.")
+        for chain in self.__buckets:
+            for link in chain:
+                yield link.val
 
     def items(self) -> Iterator[Tuple[KT, VT]]:
-        raise NotImplementedError("HashMap.items() is not implemented yet.")
+        for chain in self.__buckets:
+            for link in chain:
+                yield (link.key, link.val)
             
     def __delitem__(self, key: KT) -> None:
-        raise NotImplementedError("HashMap.__delitem__() is not implemented yet.")
+        chain: LinkedList[HashMap.ChainLink] = self.__buckets[self.__get_bucket_index(key)]
+        val = self[key]
+        chain.remove(HashMap.ChainLink(key, val))
+        self.__count -= 1
     
     def __contains__(self, key: KT) -> bool:
-        raise NotImplementedError("HashMap.__contains__() is not implemented yet.")
+        chain: LinkedList[HashMap.ChainLink] = self.__buckets[self.__get_bucket_index(key)]
+        for link in chain:
+            if link.key == key:
+                return True
+        return False
     
     def __len__(self) -> int:
-        raise NotImplementedError("HashMap.__len__() is not implemented yet.")
+        return self.__count
     
     def __iter__(self) -> Iterator[KT]:
-        raise NotImplementedError("HashMap.__iter__() is not implemented yet.")
+        for chain in self.__buckets:
+            for link in chain:
+                yield link.key
     
     def __eq__(self, other: object) -> bool:
         raise NotImplementedError("HashMap.__eq__() is not implemented yet.")
@@ -47,9 +101,31 @@ class HashMap(IHashMap[KT, VT]):
     
     def __repr__(self) -> str:
         return f"HashMap({str(self)})"
+    
+    def __resize(self, number_of_buckets: Optional[int] = None):
+        #Find the next prime that's at least double the current size
+        if number_of_buckets is None:
+            working_prime = next(self.__prime_iter)
+            while working_prime < 2*len(self.__buckets):
+                working_prime = next(self.__prime_iter)
+            number_of_buckets = working_prime
+        
+        new_buckets: Array[LinkedList[HashMap.ChainLink]] = \
+            Array([LinkedList(data_type=HashMap.ChainLink) for _ in range(number_of_buckets)], data_type=LinkedList)
+        
+        for key, val in self.items():
+            new_buckets[self.__get_bucket_index(key, number_of_buckets)].append(HashMap.ChainLink(key, val))
 
+    def __get_bucket_index(self, key: KT, num_buckets: Optional[int] = None) -> int:
+        """
+        Helper function to convert key to bucket index
+        Uses len(self) if num_buckets is None
+        """
+        bucket_index = self.__hash_function(key)
+        return bucket_index % (num_buckets if num_buckets is not None else len(self.__buckets))
+    
     @staticmethod
-    def _default_hash_function(key: KT) -> int:
+    def __default_hash_function(key: KT) -> int:
         """
         Default hash function for the HashMap.
         Uses Pickle to serialize the key and then hashes it using SHA-256. 
